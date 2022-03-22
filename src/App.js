427b24b2c41b5dba3d590417e95d52802907f776
unknown <gmgustavom@gmail.com>
2022-03-22 17:24:23 -0300

    feat: getting Planet List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getPlanets from './services/api';
 import './App.css';
 
 function App() {
+  const [planetList, updatePlanetList] = useState([]);
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const data = await getPlanets();
+      updatePlanetList(data);
+    };
+
+    fetchPlanets();
+  }, [updatePlanetList]);
+
   return (
     <span>Hello, App!</span>
   );
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const getPlanets = async () => {
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const response = await fetch(url);
+  const { results } = await response.json();
+  return results;
+};
+
+export default getPlanets;
