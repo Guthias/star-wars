91335f47f32d6d325300d7eac32c9d3c7ad37ed3
unknown <gmgustavom@gmail.com>
2022-03-25 13:59:00 -0300

    feat: changing NumericFilter values on local state

--- src/components/TableFilters/NumericFilter.jsx
+++ src/components/TableFilters/NumericFilter.jsx
@@ -1,19 +1,38 @@
-import { object } from 'prop-types';
 import React, { useState } from 'react';
 import { usePlanets } from '../../context/PlanetsProvider';
 
 export default function NumericFilter() {
+  // Getting keys with numeric Values
   const { planets } = usePlanets();
-
   const numberKeys = planets
     ? Object.keys(planets[0]).filter((key) => !Number.isNaN(+planets[0][key]))
     : [];
+
+  const INITIAL_STATE = {
+    column: '',
+    comparsion: '',
+    value: '',
+  };
+
+  const [filterValues, setFilterValues] = useState(INITIAL_STATE);
+
+  const changeFilters = ({ target }) => {
+    setFilterValues({
+      ...filterValues,
+      [target.name]: target.value,
+    });
+  };
 
   return (
     <div>
       <label htmlFor="column-filter">
         <span>Coluna</span>
-        <select id="column-filter" data-testid="column-filter" name="column">
+        <select
+          id="column-filter"
+          data-testid="column-filter"
+          name="column"
+          onChange={ changeFilters }
+        >
           {
             numberKeys.map((numericKey) => (
               <option key={ numericKey } value={ numericKey }>
@@ -23,9 +42,14 @@ export default function NumericFilter() {
         </select>
       </label>
 
-      <label htmlFor="column-filter">
+      <label htmlFor="comparison-filter">
         <span>Coluna</span>
-        <select id="column-filter" data-testid="column-filter" name="column">
+        <select
+          id="comparison-filter"
+          data-testid="comparison-filter"
+          name="comparsion"
+          onChange={ changeFilters }
+        >
           <option value="menor que">menor que</option>
           <option value="maior que">maior que</option>
           <option value="igual a">igual a</option>
@@ -34,7 +58,12 @@ export default function NumericFilter() {
 
       <label htmlFor="value-filter">
         <span>Coluna</span>
-        <input data-testid="value-fiter" name="value" type="number" />
+        <input
+          data-testid="value-fiter"
+          name="value"
+          type="number"
+          onChange={ changeFilters }
+        />
       </label>
 
       <button type="submit" data-testid="button-filter">Filtrar</button>
